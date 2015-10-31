bbc8e5ff2881949782d7cd4f744893a2da09bd55
Sayanna <sayanna@gmail.com>
2015-10-31 13:09:14 -0700

    clean-up

--- inc/main.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int insert_sort(int *input, int *output, int length);
-int selection_sort(int *input, int *output, int length);
-int bubble_sort(int *input, int *output, int length);
